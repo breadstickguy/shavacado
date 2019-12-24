58f73723526e4e11e17ccf4eb445a54c5aaa88bd
Magic-Mayo <mike.mayoii@gmail.com>
2019-12-24 16:17:05 -0700

    removed Router from import

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
 import Home from './page/Home';
